df99c4c236097d1e16422a2be7db5996c23f1215
Zaid Shaikh <zaid.shaikh@kanini.com>
2026-03-25 15:51:35 +0530

    signature service url updated

--- ang-frontend/src/environments/environment.prod.ts
+++ ang-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://fastapi-loan.onrender.com',
-  signatureServiceUrl: 'https://signature-service-khts.onrender.com'
+  signatureServiceUrl: 'https://signature-service-x5bk.onrender.com'
 };
