21ba48e009ae72045c11397f2c77b8a6ceac3d77
Zaid Shaikh <zaid.shaikh@kanini.com>
2026-03-25 17:46:42 +0530

    signature service deployed on railway

--- ang-frontend/src/environments/environment.prod.ts
+++ ang-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://fastapi-loan.onrender.com',
-  signatureServiceUrl: 'https://signature-service-x5bk.onrender.com'
+  signatureServiceUrl: 'https://signature-service-production.up.railway.app'
 };
